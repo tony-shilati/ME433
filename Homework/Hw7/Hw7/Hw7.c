c3cff503b51e2c75f5afc21fb1b45b303d1ceac4
Tony Shilati <anthonyshilati@gmail.com>
2025-04-24 12:25:30 -0500

    LED displays each ASCII character on at a time

--- Homework/Hw7/Hw7/Hw7.c
+++ Homework/Hw7/Hw7/Hw7.c
@@ -2,6 +2,7 @@
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
 #include "ssd1306.h"
+#include "font.h"
 
 // I2C defines
 // This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
@@ -10,12 +11,8 @@
 #define I2C_SDA 8
 #define I2C_SCL 9
 
-void ssd1306_draw_line(unsigned char x, unsigned char color){
-    for (int i = 0; i < 32; i++) {
-        ssd1306_drawPixel(x, i, color);
-    }
-    ssd1306_update();
-}
+void ssd1306_draw_ASCII(unsigned char x, unsigned char y, unsigned char character);
+void ssd1306_update_vertical_byte(unsigned char x, unsigned char y, unsigned char byte);
 
 
 
@@ -43,27 +40,96 @@ int main()
 
     unsigned char color = 1;
     unsigned char dir = 0;
-    unsigned char counter = 1;
+    unsigned char counter = 0x20;
     while (true) {
-        ssd1306_draw_line(counter, color);
-        
-        if (counter == 0 || counter == 127) {
-            dir = !dir;
-        }
 
-        if (dir == 0) {
-            ssd1306_draw_line(counter-1, !color);
-            counter++;
-        } else {
-            ssd1306_draw_line(counter+1, !color);
-            counter--;
+        ssd1306_clear();
+        ssd1306_update();
+        ssd1306_draw_ASCII(10, 16, counter);
+        ssd1306_update();
+
+        // Increment the counter
+        counter++;
+        if (counter > 0x7F) {
+            counter = 0;
         }
         
-
         // Flip the hearbeat LED at 1Hz
         if (to_ms_since_boot(get_absolute_time()) - start_time > 1000) { 
             start_time = to_ms_since_boot(get_absolute_time()); 
             gpio_put(25, !gpio_get(25)); 
         }
+
+        // Wait for a short time
+        sleep_ms(2000);
+    }
+}
+
+
+
+
+/*
+ * Helper function defintions
+ */
+ 
+ void ssd1306_draw_ASCII(unsigned char x, unsigned char y, unsigned char character) {
+    unsigned char current_byte;
+
+    for (int i = 0; i < 5; i++) {
+        current_byte = ASCII[character][i];
+
+        ssd1306_update_vertical_byte(x + i, y, current_byte);
+    }
+    ssd1306_update();
+}
+
+
+void ssd1306_update_vertical_byte(unsigned char x, unsigned char y, unsigned char byte){
+    if (0b1 & byte) {
+        ssd1306_drawPixel(x, y, 1);
+    } else {
+        ssd1306_drawPixel(x, y, 0);
+    }
+
+    if (0b1 << 1 & byte) {
+        ssd1306_drawPixel(x, y + 1, 1);
+    } else {
+        ssd1306_drawPixel(x, y + 1, 0);
+    }
+
+    if (0b1 << 2 & byte) {
+        ssd1306_drawPixel(x, y + 2, 1);
+    } else {
+        ssd1306_drawPixel(x, y + 2, 0);
+    }
+
+    if (0b1 << 3 & byte) {
+        ssd1306_drawPixel(x, y + 3, 1);
+    } else {
+        ssd1306_drawPixel(x, y + 3, 0);
+    }
+    
+    if (0b1 << 4 & byte) {
+        ssd1306_drawPixel(x, y + 4, 1);
+    } else {
+        ssd1306_drawPixel(x, y + 4, 0);
+    }
+
+    if (0b1 << 5 & byte) {
+        ssd1306_drawPixel(x, y + 5, 1);
+    } else {
+        ssd1306_drawPixel(x, y + 5, 0);
+    }
+
+    if (0b1 << 6 & byte) {
+        ssd1306_drawPixel(x, y + 6, 1);
+    } else {
+        ssd1306_drawPixel(x, y + 6, 0);
+    }
+
+    if (0b1 << 7 & byte) {
+        ssd1306_drawPixel(x, y + 7, 1);
+    } else {
+        ssd1306_drawPixel(x, y + 7, 0);
     }
 }
