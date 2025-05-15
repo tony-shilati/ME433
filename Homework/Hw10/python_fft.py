c27f549b83775798eb9f69d88c58e5a88c386523
Tony Shilati <anthonyshilati@gmail.com>
2025-05-14 23:05:14 -0500

    Working fft code

---
+++ Homework/Hw10/python_fft.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+
+# Read the csv file
+data = pd.read_csv('signals/sigA.csv', header=None)
+# Extract the columns
+t = data[0].values  # time
+s = data[1].values  # signal
+
+
+Fs = 10000 # sample rate
+Ts = 1.0/Fs; # sampling interval
+ts = np.arange(0,t[-1],Ts) # time vector
+y = s # the data to make the fft from
+n = len(y) # length of the signal
+k = np.arange(n)
+T = n/Fs
+frq = k/T # two sides frequency range
+frq = frq[range(int(n/2))] # one side frequency range
+Y = np.fft.fft(y)/n # fft computing and normalization
+Y = Y[range(int(n/2))]
+
+fig, (ax1, ax2) = plt.subplots(2, 1)
+ax1.plot(t,y,'b')
+ax1.set_xlabel('Time')
+ax1.set_ylabel('Amplitude')
+ax2.loglog(frq,abs(Y),'b') # plotting the fft
+ax2.set_xlabel('Freq (Hz)')
+ax2.set_ylabel('|Y(freq)|')
+plt.show()
